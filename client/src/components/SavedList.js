e438606c6c4e7594ad20f006b8ee95e9ac13f563
David Mostoller <dmostoller@gmail.com>
2024-05-08 21:27:02 -0400

    saved stackable

--- client/src/components/SavedList.js
+++ client/src/components/SavedList.js
@@ -15,7 +15,7 @@ export default function SavedList({savedItems, onDeleteSaved}) {
     })
     return (
         <div className="ui centered grid" style={{marginBottom: "25px"}}>
-            <div class="ui inverted centered five link cards" >
+            <div class="ui five stackable inverted centered link cards" >
                 {favorites}
             </div> 
         </div>
