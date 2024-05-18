57e5b59d44cfb95d8625fcef06b04fdb9ac24be4
David Mostoller <dmostoller@gmail.com>
2024-05-18 17:59:27 -0400

    added chatbot button

--- client/src/components/Learn.js
+++ client/src/components/Learn.js
@@ -2,14 +2,7 @@
 import React, { useState } from "react"
 import Tutorials from "./Tutorials";
 import GigVideos from "./GigVideos";
-
-// const YOUTUBE_PLAYLIST_ITEMS_API = 'https://www.googleapis.com/youtube/v3/playlistItems';
-
-// export async function getServerSideProps() {
-//     const res = await fetch(`${YOUTUBE_PLAYLIST_ITEMS_API}?part=snippet&playlistId=PLkiLSmC1caWvoUoGTKuBhbS&maxResults=50&key=${process.env.YOUTUBE_API_KEY}`);
-//     const data = await res.json();
-//     return data 
-//     }
+import Chat from "./Chat"
 
 
 function Learn() {
@@ -26,28 +19,20 @@ function Learn() {
             <div className="ui inverted centered secondary pointing menu">
                 
                 { tutVis ?
-                <a onClick={changeTab} className="active item">Tutorials</a>
+                <a onClick={changeTab} href="#tutorials" className="active item">Tutorials</a>
                 :
-                <a onClick={changeTab} className="item">Tutorials</a>
+                <a onClick={changeTab} href="#tutorials" className="item">Tutorials</a>
                 }
                 
                 
                 { !tutVis ? 
-                <a onClick={changeTab} className="active item">Music</a>
+                <a onClick={changeTab} href="#music" className="active item">Music</a>
                 :
-                <a onClick={changeTab} className="item">Music</a>
+                <a onClick={changeTab} href="#music" className="item">Music</a>
                 }
             
             </div>
             <div className="ui middle aligned center aligned grid" style={{padding: "10px", minHeight:"100vh"}}>
-
-            {/* <div style={{padding:"56.25% 0 0 0", position:"relative"}}>
-                <iframe src='https://vimeo.com/showcase/11038900/embed' 
-                allowfullscreen 
-                frameborder='0' 
-                style={{position: "absolute", top:"0", left:"0", width:"100%", height:"100%"}}>
-                </iframe>
-            </div> */}
             {tutVis ?
                 <Tutorials/>
             :
@@ -55,6 +40,11 @@ function Learn() {
             }
             </div>    
         </div>
+        {tutVis &&
+            <div style={{ position:"relative", height: "500px" }}>
+              <Chat />
+            </div>
+        }
         </>
 
     )
