4779e9df66e9b3ad6671c0d9a0b3fbb68aeef078
David Mostoller <dmostoller@gmail.com>
2024-06-15 03:51:55 -0400

    button

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -20,7 +20,7 @@ const Player = ({track}) => {
     <table width="100%">
         <tr>
         <td width="30px">
-            <button style={{float: "left"}} className='ui circular icon button secondary mini' onClick={onPlayPause}>
+            <button style={{float: "left"}} className='ui circular icon button secondary small' onClick={onPlayPause}>
                 { isPlaying ? <i className='pause icon'></i> : <i className='play icon'></i> }
             </button>
         </td>
