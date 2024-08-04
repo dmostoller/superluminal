5e3ccb06509067c6f006a4e35cb77f57529834de
David Mostoller <dmostoller@gmail.com>
2024-08-04 16:37:32 -0400

    mobile menu

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -26,11 +26,11 @@ function Nav({onLogout}) {
 
 
     return (
-        <Menu className='ui top fixed inverted menu' style={{marginBottom: "25px"}}>
+        <Menu className='ui top fixed inverted borderless menu large' style={{marginBottom: "25px"}}>
         {(deviceSize <= 768) && 
             <>
             <Dropdown     
-            icon='inverted hamburger'
+            icon='large inverted hamburger'
             style={{padding: "1em"}}
             floating
             className='inverted fluid dropdown icon'>
@@ -38,18 +38,18 @@ function Nav({onLogout}) {
                     <Dropdown item
                         className='inverted dropdown'
                         style={{padding: "1em"}}
-                        text='Music'
+                        text="Music"
                         >
                         <DropdownMenu>
-                            <NavLink className="item" to='/music/superluminal'>Superluminal</NavLink>
-                            <NavLink className="item" to='/music/kabayun'>Kabayun</NavLink>
+                            <NavLink className="item" to='/music/superluminal'><h4>Superluminal</h4></NavLink>
+                            <NavLink className="item" to='/music/kabayun'><h4>Kabayun</h4></NavLink>
                         </DropdownMenu>
                     </Dropdown>
-                    <NavLink className="item"  to='/learn'>Video</NavLink>
-                    <NavLink className="item" to='/events'>Events</NavLink>
-                    <NavLink className="item" to='/about'>Bio</NavLink>
-                    <NavLink className="item" to='/forum'>Forum</NavLink>
-                    <a href='https://kabayun.bandcamp.com/' className='item' target='_blank' rel="noreferrer">Shop</a>  
+                    <NavLink className="item"  to='/learn'><h4>Video</h4></NavLink>
+                    <NavLink className="item" to='/events'><h4>Events</h4></NavLink>
+                    <NavLink className="item" to='/about'><h4>Bio</h4></NavLink>
+                    <NavLink className="item" to='/forum'><h4>Forum</h4></NavLink>
+                    <a href='https://kabayun.bandcamp.com/' className='item' target='_blank' rel="noreferrer"><h4>Shop</h4></a>  
                 </DropdownMenu>
             </Dropdown>
             </>
@@ -83,20 +83,20 @@ function Nav({onLogout}) {
             <div className="item">
                 { !user ? (
                     <>
-                    <Link to='/login' style={{marginRight: '3px'}} data-inverted="" data-tooltip="Login" data-position="bottom center" className="ui circular violet icon button small">
+                    <Link to='/login' style={{marginRight: '3px'}} data-inverted="" data-tooltip="Login" data-position="bottom center" className="ui circular violet icon button">
                         <i className="sign in alternate icon"></i>
                     </Link>
-                    <Link to='/signup' style={{marginRight: '3px'}} data-inverted="" data-tooltip="Sign Up" data-position="bottom right" className="ui circular violet icon button small">
+                    <Link to='/signup' style={{marginRight: '3px'}} data-inverted="" data-tooltip="Sign Up" data-position="bottom right" className="ui circular violet icon button">
                         <i className="plus icon"></i>
                     </Link>
                     </>
                 ) : (
                     <>
-                    <button onClick={handleLogout} style={{marginRight: '3px'}} data-inverted="" data-tooltip="Logout" data-position="bottom center" className="ui circular violet icon button small">
+                    <button onClick={handleLogout} style={{marginRight: '3px'}} data-inverted="" data-tooltip="Logout" data-position="bottom center" className="ui circular violet icon button">
                         <i className="sign out alternate icon"></i>
                     </button>
                     {isAdmin &&
-                    <Link to='/dashboard' style={{marginRight: '3px'}} data-inverted="" data-tooltip="Dashboard" data-position="bottom center" className="ui circular violet icon button small">
+                    <Link to='/dashboard' style={{marginRight: '3px'}} data-inverted="" data-tooltip="Dashboard" data-position="bottom center" className="ui circular violet icon button">
                         <i className="chartline icon"></i>
                     </Link>
                     }
